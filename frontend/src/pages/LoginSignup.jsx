777404a23f714c20cfd1e08b640c82dc16a66ee2
AakashKale17 <128164804+AakashKale17@users.noreply.github.com>
2024-01-13 11:52:10 +0000

    "backend and frontend v1"

---
+++ frontend/src/pages/LoginSignup.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import './LoginSignup.css';
+import { useNavigate } from 'react-router-dom';
+
+function LoginSignup() {
+  const [action, setAction] = useState("Sign up");
+  const navigate = useNavigate();
+  const Click = () => {
+		navigate('/', {replace: true});
+	} 
+
+  return (
+    <div className='container'>
+    <button onClick={Click}>Back</button>
+    <div className="header">
+    <h3>XYZ WEBSITE</h3>
+    <div className="text">{action}</div>
+    </div>
+    <div className="inputs">
+      {action==='Sign In' ? <div></div>:<div className='input'>
+   
+   <input type='text' placeholder='Enter Name...'/>
+ </div>}
+    <div className='input'>
+
+    <input type='email' placeholder='Enter Email...'/>
+    </div>
+    <div className='input'>
+
+    <input type='password' placeholder='Enter Password...'/>
+    </div>
+    </div>
+    <div className="submit-container">
+      <div className="submit" onClick={()=>{setAction('Sign Up')}}>Sign up</div>
+      <div className="submit" onClick={()=>{setAction('Sign In')}}>Sign In</div> 
+    </div>
+    {action==="Sign Up"?<div></div>:     <div className="forgot-pass">Forgot Password? <span>click here</span></div>
+}
+    </div>
+  )
+}
+
+export default LoginSignup;
